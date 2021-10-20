94b2370f54afdbf1c54f795c51fb8dea05e9ed57
Cruelkid <sonofammit@gmail.com>
2021-10-20 22:55:28 +0300

    added links

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -3,9 +3,9 @@ import { useRouter } from "next/router";
 const DetailPage = () => {
     const router = useRouter();
 
-    console.log(router.query.newsId);
+    const newsId = router.query.newsId;
 
-    return <h1>News page</h1>;
+    return <h1>{newsId} Detail page</h1>;
 };
 
 export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 const NewsPage = () => {
-    return <h1>News page</h1>;
+    return (
+        <Fragment>
+            <h1>News page</h1>
+            <ul>
+                <li><Link href='/news/first-item'>Item 1</Link></li>
+                <li><Link href='/news/second-item'>Item 2</Link></li>
+            </ul>
+        </Fragment>
+    );
 };
 
 export default NewsPage;
